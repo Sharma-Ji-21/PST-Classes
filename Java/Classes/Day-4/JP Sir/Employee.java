b6b29f60a59709455ec442341fd912e1ce0a8bc1
Om Sharma <justsharmaji69@gmail.com>
2024-10-18 01:11:04 +0530

    JP Sir

---
+++ Java/Classes/Day-4/JP Sir/Employee.java
@@ -0,0 +1,11 @@
+package people;
+import people.Person;
+public class  Employee extends Person{
+
+
+ public Employee(){
+     System.out.println("child class :"+name);
+     System.out.println("child class :"+age);
+     // System.out.println("child class :"+Atmpin); (Private)
+}
+}
