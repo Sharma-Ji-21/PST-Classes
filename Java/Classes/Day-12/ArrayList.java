27b3cdf15bc8e31fe2de9b21765b7a98ea2c645f
Om Sharma <justsharmaji69@gmail.com>
2024-11-06 09:08:51 +0530

    Array List

---
+++ Java/Classes/Day-12/ArrayList.java
@@ -0,0 +1,14 @@
+/*
+ARRAY LIST:
+    -Dynami (can change there size any time)
+    name.add(2) {this will add things in array}
+    name.remove(index) {delete}
+    name.set(index,value) {update}
+    name.size() {lenght}
+    name.clear() {all clear}
+    syantax:
+    Arraylist<Integer> name=new Arraylist<>().
+*/
+public class ArrayList {
+    
+}
