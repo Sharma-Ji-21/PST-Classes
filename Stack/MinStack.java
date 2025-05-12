0a21a1ad321d88222d7928b1b3dba3c04776f066
Om Sharma <justsharmaji69@gmail.com>
2025-05-12 11:00:36 +0530

    Stack and Tree

---
+++ Stack/MinStack.java
@@ -0,0 +1,14 @@
+class Node{
+    int val;
+    Node next;
+    Node prev;
+    Node(int x) {
+        val=x;
+        next=null;
+        prev=null;
+    }
+}
+class MinStack {
+    
+
+}
---
+++ Tree/leftView.java
@@ -0,0 +1,6 @@
+public class leftView {
+    public static void main(String arg[]){
+        
+    }
+    
+}
